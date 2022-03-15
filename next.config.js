51cba2553099dc04cfcf29dc9586cbef6ac46bda
EmilLM <luchianemil@gmail.com>
2022-03-15 11:22:56 +0200

    working queries with next 12

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   reactStrictMode: true,
-  // compiler: {
-  //   relay: {
-  //     // This should match relay.config.js
-  //     src: "./src",
-  //     schema: "./src/schemas/schema.graphql",
-  //     // artifactDirectory: "./src/gql/__generated__",
-  //     // language: 'typescript',
-  //     exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
-  //   },
-  // },
+  compiler: {
+    relay: {
+      // This should match relay.config.js
+      src: "./src",
+      schema: "./src/schemas/schema.graphql",
+      // artifactDirectory: "./src/gql/__generated__",
+      // language: 'typescript',
+      exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+    },
+  },
 }
